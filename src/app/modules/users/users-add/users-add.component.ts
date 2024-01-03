46c8f2056ac48c6a4a95691d0731ede2b06b451a
Rjchi <richardospina18@gmail.com>
2024-01-03 18:34:28 -0500

    [Feat] Agregamos la previsualización de un avatar para un usuario en especifico

--- src/app/modules/users/users-add/users-add.component.ts
+++ src/app/modules/users/users-add/users-add.component.ts
@@ -1,24 +1,63 @@
 import { Component, OnInit } from '@angular/core';
+import { Toaster } from 'ngx-toast-notifications';
 
 @Component({
   selector: 'app-users-add',
   templateUrl: './users-add.component.html',
-  styleUrls: ['./users-add.component.scss']
+  styleUrls: ['./users-add.component.scss'],
 })
 export class UsersAddComponent implements OnInit {
+  rol: string = 'admin';
 
-  rol: string = "admin";
+  name: string = '';
+  email: string = '';
+  surname: string = '';
+  password: string = '';
+  profession: string = '';
+  description: string = '';
 
-  name: string = "";
-  email: string = "";
-  surname: string = "";
-  password: string = "";
-  profession: string = "";
-  description: string = "";
+  /**------------------------------------------------------
+   * | Aqui asignamos la imagen que queremos previsualizar
+   * | De manera dinamica
+   * ------------------------------------------------------*/
+  FILE_AVATAR: any;
+  IMAGEN_PREVIZUALIZAR: any = 'assets/media/avatars/300-6.jpg';
 
-  constructor() { }
+  /**----------------------------------------------------
+   * | Toaster lo utilizamos para las notificaciones
+   * | Emergentes
+   * ----------------------------------------------------*/
+  constructor(public toaster: Toaster) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  processAvatar($event: any) {
+    /**----------------------------------------------------------------
+     * | La imagen queda añadida como el primer elemento de un array
+     * | Validamos si contiene un string que diga 'image'
+     * | En caso de que la salida sea -1 significa que no es una img
+     * ----------------------------------------------------------------*/
+    if ($event.target.files[0].type.indexOf('image') < 0) {
+      /**---------------------------------------------------
+       * | Aperturamos la notificación flotante
+       * | parametros: MENSAJE, TITULO, DISEÑO
+       * ---------------------------------------------------*/
+      this.toaster.open({
+        text: 'SOLAMENTE SE ACEPTA IMAGENES',
+        caption: 'VALIDACIONES',
+        type: 'danger',
+      });
+      return;
+    }
+
+    this.FILE_AVATAR = $event.target.files[0]; // Asignamos el archivo
+
+    /**--------------------------------------------------------------
+     * | Convertimos el archivo a base 64 para poder previsualizar
+     * | La imagen
+     * --------------------------------------------------------------*/
+    let reader = new FileReader();
+    reader.readAsDataURL(this.FILE_AVATAR);
+    reader.onloadend = () => (this.IMAGEN_PREVIZUALIZAR = reader.result);
   }
-
 }
