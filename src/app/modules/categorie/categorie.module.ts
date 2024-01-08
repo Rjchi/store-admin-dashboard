35fa0ce87145fa18b98106d42caea0e96013b885
Rjchi <richardospina18@gmail.com>
2024-01-08 17:56:01 -0500

    [Feat] Registrar componentes add, edit, delete y list

--- src/app/modules/categorie/categorie.module.ts
+++ src/app/modules/categorie/categorie.module.ts
@@ -3,11 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { CategorieRoutingModule } from './categorie-routing.module';
 import { CategorieComponent } from './categorie.component';
+import { CategorieListComponent } from './categorie-list/categorie-list.component';
+import { CategorieAddComponent } from './categorie-add/categorie-add.component';
+import { CategorieEditComponent } from './categorie-edit/categorie-edit.component';
+import { CategorieDeleteComponent } from './categorie-delete/categorie-delete.component';
 
 
 @NgModule({
   declarations: [
-    CategorieComponent
+    CategorieComponent,
+    CategorieListComponent,
+    CategorieAddComponent,
+    CategorieEditComponent,
+    CategorieDeleteComponent
   ],
   imports: [
     CommonModule,
