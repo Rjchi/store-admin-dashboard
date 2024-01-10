8e29335f49e77d4b215240f1a6f7ec55d0a82ecc
Rjchi <richardospina18@gmail.com>
2024-01-09 21:08:56 -0500

    [Refactor] Modificar el componente categorie-add para que sea funcional

--- src/app/modules/categorie/categorie-add/categorie-add.component.ts
+++ src/app/modules/categorie/categorie-add/categorie-add.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Toaster } from 'ngx-toast-notifications';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+import { CategorieService } from '../service/categorie.service';
 
 @Component({
   selector: 'app-categorie-add',
@@ -7,9 +11,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategorieAddComponent implements OnInit {
 
-  constructor() { }
+  @Output() CategorieC: EventEmitter<any> = new EventEmitter();
 
-  ngOnInit(): void {
+  title: string = '';
+
+  FILE_IMAGEN: any;
+  IMAGEN_PREVIZUALIZAR: any = 'assets/media/avatars/300-6.jpg';
+
+  constructor(
+    public toaster: Toaster,
+    public modal: NgbActiveModal,
+    public categorieService: CategorieService,
+  ) {}
+
+  ngOnInit(): void {}
+
+  processAvatar($event: any) {
+    if ($event.target.files[0].type.indexOf('image') < 0) {
+
+      this.toaster.open({
+        text: 'SOLAMENTE SE ACEPTA IMAGENES',
+        caption: 'VALIDACIONES',
+        type: 'danger',
+      });
+      return;
+    }
+
+    this.FILE_IMAGEN = $event.target.files[0];
+
+    let reader = new FileReader();
+    reader.readAsDataURL(this.FILE_IMAGEN);
+    reader.onloadend = () => (this.IMAGEN_PREVIZUALIZAR = reader.result);
+  }
+
+  save() {
+    if (!this.title || !this.FILE_IMAGEN) {
+      this.toaster.open({
+        text: 'NECESITAS INGRESAR TODOS LOS CAMPOS',
+        caption: 'VALIDACIÓN',
+        type: 'danger',
+      });
+
+      return;
+    }
+
+    let formData = new FormData();
+
+    formData.append('title', this.title);
+    formData.append('avatar', this.FILE_IMAGEN);
+
+    this.categorieService.registerCategorie(formData).subscribe((res: any) => {
+
+      this.CategorieC.emit(res.categorie);
+      this.modal.close();
+
+      this.toaster.open({
+        text: 'SE REGISTRO UNA NUEVA CATEGORIA',
+        caption: 'VALIDACIÓN',
+        type: 'primary',
+      });
+    });
   }
 
 }
