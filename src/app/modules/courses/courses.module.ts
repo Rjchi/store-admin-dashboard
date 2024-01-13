83b621a46b43ca20c2ea6d202389421ee8942e51
Rjchi <richardospina18@gmail.com>
2024-01-12 22:57:09 -0500

    [Feat] Registrar los componentes add, edit, list y delete al modulo courses

--- src/app/modules/courses/courses.module.ts
+++ src/app/modules/courses/courses.module.ts
@@ -3,11 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses.component';
+import { CoursesAddComponent } from './courses-add/courses-add.component';
+import { CoursesEditComponent } from './courses-edit/courses-edit.component';
+import { CoursesListComponent } from './courses-list/courses-list.component';
+import { CoursesDeleteComponent } from './courses-delete/courses-delete.component';
 
 
 @NgModule({
   declarations: [
-    CoursesComponent
+    CoursesComponent,
+    CoursesAddComponent,
+    CoursesEditComponent,
+    CoursesListComponent,
+    CoursesDeleteComponent
   ],
   imports: [
     CommonModule,
