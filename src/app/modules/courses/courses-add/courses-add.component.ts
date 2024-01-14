db6374d7948dcf473686ecfafb6ad76c312abcb1
Rjchi <richardospina18@gmail.com>
2024-01-13 22:24:24 -0500

    [Feat] Agregar estructura inicial del componente de registro de un nuevo curso

--- src/app/modules/courses/courses-add/courses-add.component.ts
+++ src/app/modules/courses/courses-add/courses-add.component.ts
@@ -12,4 +12,22 @@ export class CoursesAddComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  processFile($event: any){
+    // if ($event.target.files[0].type.indexOf('image') < 0) {
+
+    //   this.toaster.open({
+    //     text: 'SOLAMENTE SE ACEPTA IMAGENES',
+    //     caption: 'VALIDACIONES',
+    //     type: 'danger',
+    //   });
+    //   return;
+    // }
+
+    // this.FILE_IMAGEN = $event.target.files[0];
+
+    // let reader = new FileReader();
+    // reader.readAsDataURL(this.FILE_IMAGEN);
+    // reader.onloadend = () => (this.IMAGEN_PREVIZUALIZAR = reader.result);
+  }
+
 }
