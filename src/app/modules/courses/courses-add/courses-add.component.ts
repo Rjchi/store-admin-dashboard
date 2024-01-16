a163da9adf0065e67b4534bef9e9898ed654fcd8
Rjchi <richardospina18@gmail.com>
2024-01-15 19:59:02 -0500

    [Feat] Implementar el enpoint que trae categorias y usuarios

--- src/app/modules/courses/courses-add/courses-add.component.ts
+++ src/app/modules/courses/courses-add/courses-add.component.ts
@@ -1,20 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CoursesService } from '../service/courses.service';
+
 @Component({
   selector: 'app-courses-add',
   templateUrl: './courses-add.component.html',
-  styleUrls: ['./courses-add.component.scss']
+  styleUrls: ['./courses-add.component.scss'],
 })
 export class CoursesAddComponent implements OnInit {
+  USERS: any = [];
+  CATEGORIES: any = [];
+  isLoading$: any; // Siempre es neccesario para que se renderice correctamente la vista
 
-  constructor() { }
+  constructor(public coursesService: CoursesService) {}
 
   ngOnInit(): void {
+    this.isLoading$ = this.coursesService.isLoading$;
+
+    this.coursesService.configAll().subscribe((res: any) => {
+      this.USERS = res.users;
+      this.CATEGORIES = res.categories;
+    });
   }
 
-  processFile($event: any){
+  processFile($event: any) {
     // if ($event.target.files[0].type.indexOf('image') < 0) {
-
     //   this.toaster.open({
     //     text: 'SOLAMENTE SE ACEPTA IMAGENES',
     //     caption: 'VALIDACIONES',
@@ -22,12 +32,9 @@ export class CoursesAddComponent implements OnInit {
     //   });
     //   return;
     // }
-
     // this.FILE_IMAGEN = $event.target.files[0];
-
     // let reader = new FileReader();
     // reader.readAsDataURL(this.FILE_IMAGEN);
     // reader.onloadend = () => (this.IMAGEN_PREVIZUALIZAR = reader.result);
   }
-
 }
