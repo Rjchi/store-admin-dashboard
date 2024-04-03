edfdfd6536ed3d82c24e86dfc17a488d1c009088
Rjchi <richardospina18@gmail.com>
2024-04-03 15:48:37 -0500

    [Feat] Agregar componente para eliminar una sección de un componente

---
+++ src/app/modules/courses/sections/section-delete/section-delete.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-section-delete',
+  templateUrl: './section-delete.component.html',
+  styleUrls: ['./section-delete.component.scss']
+})
+export class SectionDeleteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
