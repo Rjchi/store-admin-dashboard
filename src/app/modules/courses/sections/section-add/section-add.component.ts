0ba3ad62616906010e111f89e20e62e7ce2db114
Rjchi <richardospina18@gmail.com>
2024-04-08 15:12:12 -0500

    [Feat] Implementar el registro de una sección

--- src/app/modules/courses/sections/section-add/section-add.component.ts
+++ src/app/modules/courses/sections/section-add/section-add.component.ts
@@ -1,3 +1,5 @@
+import { Toaster } from 'ngx-toast-notifications';
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 import { CourseSectionService } from '../../service/course-section.service';
@@ -5,16 +7,62 @@ import { CourseSectionService } from '../../service/course-section.service';
 @Component({
   selector: 'app-section-add',
   templateUrl: './section-add.component.html',
-  styleUrls: ['./section-add.component.scss']
+  styleUrls: ['./section-add.component.scss'],
 })
 export class SectionAddComponent implements OnInit {
   isLoading$: any;
   title: string;
+  courseId: string = '';
 
-  constructor(public courseSectionService: CourseSectionService) { }
+  constructor(
+    public toaster: Toaster,
+    public activatedRouter: ActivatedRoute,
+    public courseSectionService: CourseSectionService
+  ) {}
 
   ngOnInit(): void {
     this.isLoading$ = this.courseSectionService.isLoading$;
+
+    /**----------------------------
+     * | Tomamos el ID del curso
+     * ----------------------------*/
+    this.activatedRouter.params.subscribe((resp: any) => {
+      this.courseId = resp.id;
+    });
   }
-save(){}
+
+  save() {
+    if (!this.title) {
+      this.toaster.open({
+        text: 'EL TITULO ES REQUERIDO PARA REGISTRAR UNA SECCIÓN',
+        caption: 'VALIDACIÓN',
+        type: 'danger',
+      });
+      return;
+    }
+
+    let data = {
+      course: this.courseId,
+      title: this.title,
+    };
+
+    this.courseSectionService.registerSection(data).subscribe((resp: any) => {
+      console.log(resp);
+      if (resp.msg === 403) {
+        this.toaster.open({
+          text: resp.message_text,
+          caption: 'VALIDACIÓN',
+          type: 'danger',
+        });
+      } else {
+        this.title = "";
+
+        this.toaster.open({
+          text: 'LA SECCIÓN SE HA REGISTRADO CORRECTAMENTE',
+          caption: 'VALIDACIÓN',
+          type: 'primary',
+        });
+      }
+    });
+  }
 }
