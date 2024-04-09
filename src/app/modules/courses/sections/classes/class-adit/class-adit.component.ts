2a908331211e3af90bcffb02f78ccc1dfd152b04
Rjchi <richardospina18@gmail.com>
2024-04-09 10:06:25 -0500

    [Feat] Agregar componente para editar una clase

---
+++ src/app/modules/courses/sections/classes/class-adit/class-adit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-class-adit',
+  templateUrl: './class-adit.component.html',
+  styleUrls: ['./class-adit.component.scss']
+})
+export class ClassAditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
