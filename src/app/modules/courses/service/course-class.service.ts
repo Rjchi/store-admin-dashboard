b921bdd792c466b2611415a03b43d4edbfffc559
Rjchi <richardospina18@gmail.com>
2024-04-09 15:38:22 -0500

    [Feat] Agregar metodos para trabajar con las clases de una sección

--- src/app/modules/courses/service/course-class.service.ts
+++ src/app/modules/courses/service/course-class.service.ts
@@ -1,9 +1,64 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, BehaviorSubject, finalize } from 'rxjs';
+
+import { AuthService } from '../../auth';
+import { URL_SERVICIOS } from 'src/app/config/config';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CourseClassService {
+  isLoading$: Observable<boolean>;
+  isLoadingSubject: BehaviorSubject<boolean>;
 
-  constructor() { }
+  constructor(private http: HttpClient, public authservice: AuthService) {
+    this.isLoadingSubject = new BehaviorSubject<boolean>(false);
+    this.isLoading$ = this.isLoadingSubject.asObservable();
+  }
+
+  listClasses() {
+    this.isLoadingSubject.next(true);
+
+    let headers = new HttpHeaders({ token: this.authservice.token });
+
+    let URL = URL_SERVICIOS + '/course-class/list';
+
+    return this.http
+      .get(URL, { headers })
+      .pipe(finalize(() => this.isLoadingSubject.next(false)));
+  }
+
+  registerClass(data: any) {
+    this.isLoadingSubject.next(true);
+
+    let headers = new HttpHeaders({ token: this.authservice.token });
+    let URL = URL_SERVICIOS + '/course-class/register';
+
+    return this.http
+      .post(URL, data, { headers })
+      .pipe(finalize(() => this.isLoadingSubject.next(false)));
+  }
+
+  updateClass(data: any) {
+    this.isLoadingSubject.next(true);
+
+    let headers = new HttpHeaders({ token: this.authservice.token });
+    let URL = URL_SERVICIOS + '/course-class/update';
+
+    return this.http
+      .put(URL, data, { headers })
+      .pipe(finalize(() => this.isLoadingSubject.next(false)));
+  }
+
+  removeClass(section_id: any) {
+    this.isLoadingSubject.next(true);
+
+    let headers = new HttpHeaders({ token: this.authservice.token });
+    let URL = URL_SERVICIOS + '/course-class/remove/' + section_id;
+
+    return this.http
+      .delete(URL, { headers })
+      .pipe(finalize(() => this.isLoadingSubject.next(false)));
+  }
 }
--- src/app/modules/courses/service/course-section.service.ts
+++ src/app/modules/courses/service/course-section.service.ts
@@ -1,4 +1,3 @@
-import { state } from '@angular/animations';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, BehaviorSubject, finalize } from 'rxjs';
