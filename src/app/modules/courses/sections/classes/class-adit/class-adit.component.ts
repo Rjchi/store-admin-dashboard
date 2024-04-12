c6666b9da5059a65e37bad6f41eb73877d7f0a34
Rjchi <richardospina18@gmail.com>
2024-04-12 11:43:58 -0500

    [Feat] Agregar funcionalidad a la edición de un curso

--- src/app/modules/courses/sections/classes/class-adit/class-adit.component.ts
+++ src/app/modules/courses/sections/classes/class-adit/class-adit.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Toaster } from 'ngx-toast-notifications';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+import { CourseClassService } from '../../../service/course-class.service';
 
 @Component({
   selector: 'app-class-adit',
@@ -6,10 +10,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./class-adit.component.scss']
 })
 export class ClassAditComponent implements OnInit {
+  @Input() CLASS: any;
+  @Output() ClassE: EventEmitter<any> = new EventEmitter();
 
-  constructor() { }
+  title: string = '';
+  state: number = 1;
+  description: any = '';
+
+  constructor(
+    public modal: NgbActiveModal,
+    public toaster: Toaster,
+    public courseClassService: CourseClassService
+  ) {}
 
   ngOnInit(): void {
+    this.title = this.CLASS.title;
+    this.state = this.CLASS.state;
+    this.description = this.CLASS.description;
   }
 
+  onChange($event: any) {
+    this.description = $event.editor.getData();
+  }
+
+  save() {
+    if (!this.title) {
+      this.toaster.open({
+        text: 'EL TITULO ES REQUERIDO PARA EDITAR LA CLASE',
+        caption: 'VALIDACIÓN',
+        type: 'danger',
+      });
+      return;
+    }
+
+    let data = {
+      title: this.title,
+      state: this.state,
+      description: this.description,
+      _id: this.CLASS._id,
+    };
+
+    this.courseClassService.updateClass(data).subscribe((resp: any) => {
+      if (resp.msg === 403) {
+        this.toaster.open({
+          text: resp.message_text,
+          caption: 'VALIDACIÓN',
+          type: 'danger',
+        });
+      } else {
+        this.modal.close();
+        this.ClassE.emit(resp.course_class);
+
+        this.toaster.open({
+          text: 'LA CLASE SE HA EDITADO CORRECTAMENTE',
+          caption: 'VALIDACIÓN',
+          type: 'primary',
+        });
+      }
+    });
+  }
 }
