64a5cf18465c092a5325ba4334b6e47b56521cbf
Rjchi <richardospina18@gmail.com>
2024-04-12 11:56:23 -0500

    [Feat] Agregar la funcionalidad de eliminar una clase

--- src/app/modules/courses/sections/classes/class-delete/class-delete.component.ts
+++ src/app/modules/courses/sections/classes/class-delete/class-delete.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Toaster } from 'ngx-toast-notifications';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+import { CourseClassService } from '../../../service/course-class.service';
 
 @Component({
   selector: 'app-class-delete',
@@ -6,10 +10,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./class-delete.component.scss']
 })
 export class ClassDeleteComponent implements OnInit {
+  @Input() CLASS: any;
+  @Output() ClassD: EventEmitter<any> = new EventEmitter();
 
-  constructor() { }
+  constructor(
+    public modal: NgbActiveModal,
+    public toaster: Toaster,
+    public courseClassService: CourseClassService
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  delete() {
+    this.courseClassService.removeClass(this.CLASS._id).subscribe((res: any) => {
+      this.ClassD.emit('');
+      this.modal.close();
+
+      this.toaster.open({
+        text: 'SE ELIMINO LA CLASE CORRECTAMENTE',
+        caption: 'VALIDACIÓN',
+        type: 'primary',
+      });
+    });
   }
-
 }
