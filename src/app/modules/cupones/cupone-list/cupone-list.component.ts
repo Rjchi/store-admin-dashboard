d7e1ab6494bf92da0c7970a20b4f93004139c806
Rjchi <richardospina18@gmail.com>
2024-04-19 10:20:20 -0500

    [Feat] Agregar componente para listar los cupones

---
+++ src/app/modules/cupones/cupone-list/cupone-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cupone-list',
+  templateUrl: './cupone-list.component.html',
+  styleUrls: ['./cupone-list.component.scss']
+})
+export class CuponeListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
