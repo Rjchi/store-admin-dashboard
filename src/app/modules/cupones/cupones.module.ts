4d6988d3bc9f2b13e07eec2133e5d6ab5604a1e2
Rjchi <richardospina18@gmail.com>
2024-04-19 10:20:58 -0500

    [Feat] Registrar los componentes en el modulo cupones

--- src/app/modules/cupones/cupones.module.ts
+++ src/app/modules/cupones/cupones.module.ts
@@ -3,11 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { CuponesRoutingModule } from './cupones-routing.module';
 import { CuponesComponent } from './cupones.component';
+import { CuponeAddComponent } from './cupone-add/cupone-add.component';
+import { CuponeEditComponent } from './cupone-edit/cupone-edit.component';
+import { CuponeListComponent } from './cupone-list/cupone-list.component';
+import { CuponeDeleteComponent } from './cupone-delete/cupone-delete.component';
 
 
 @NgModule({
   declarations: [
-    CuponesComponent
+    CuponesComponent,
+    CuponeAddComponent,
+    CuponeEditComponent,
+    CuponeListComponent,
+    CuponeDeleteComponent
   ],
   imports: [
     CommonModule,
