84f17ebb96fc573e43c458673695a116c340efa4
Rjchi <richardospina18@gmail.com>
2024-04-19 10:08:39 -0500

    [Feat] Agregar el modulo de cupones al proyecto

---
+++ src/app/modules/cupones/cupones.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CuponesRoutingModule } from './cupones-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CuponesRoutingModule
+  ]
+})
+export class CuponesModule { }
