d67ccbe6783f19fe692f40f6055010b322b063a7
Rjchi <richardospina18@gmail.com>
2024-04-19 10:19:11 -0500

    [Feat] Agregar componente para registrar un cupon

---
+++ src/app/modules/cupones/cupone-add/cupone-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cupone-add',
+  templateUrl: './cupone-add.component.html',
+  styleUrls: ['./cupone-add.component.scss']
+})
+export class CuponeAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
