b88818ed5e5274e74597465947583dc55f891667
Rjchi <richardospina18@gmail.com>
2024-04-19 08:54:19 -0500

    [Feat] Agregar la opcion de eliminar un curso

--- src/app/modules/courses/courses-delete/courses-delete.component.ts
+++ src/app/modules/courses/courses-delete/courses-delete.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+import { Toaster } from 'ngx-toast-notifications';
+import { CoursesService } from '../service/courses.service';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-courses-delete',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoursesDeleteComponent implements OnInit {
 
-  constructor() { }
+  @Input() COURSE: any;
+  @Output() CourseD: EventEmitter<any> = new EventEmitter();
 
-  ngOnInit(): void {
+  constructor(
+    public toaster: Toaster,
+    public modal: NgbActiveModal,
+    public courseService: CoursesService
+  ) {}
+
+  ngOnInit(): void {}
+
+  delete() {
+    this.courseService.removeCourses(this.COURSE._id).subscribe((res: any) => {
+      this.CourseD.emit('');
+      this.modal.close();
+
+      this.toaster.open({
+        text: 'SE ELIMINO UN CURSO CORRECTAMENTE',
+        caption: 'VALIDACIÓN',
+        type: 'primary',
+      });
+    });
   }
-
 }
