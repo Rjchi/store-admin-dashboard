4318928dbd5d73a6a82b34d98eddd85eb48c3e2f
Rjchi <richardospina18@gmail.com>
2024-04-19 10:19:31 -0500

    [Feat] Agregar componente para editar un cupon

---
+++ src/app/modules/cupones/cupone-edit/cupone-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cupone-edit',
+  templateUrl: './cupone-edit.component.html',
+  styleUrls: ['./cupone-edit.component.scss']
+})
+export class CuponeEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
