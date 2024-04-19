653f54f3308fc0f84b0376528f0f3c8c33a7985f
Rjchi <richardospina18@gmail.com>
2024-04-19 10:19:51 -0500

    [Feat] Agregar componente para eliminar un cupon

---
+++ src/app/modules/cupones/cupone-delete/cupone-delete.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cupone-delete',
+  templateUrl: './cupone-delete.component.html',
+  styleUrls: ['./cupone-delete.component.scss']
+})
+export class CuponeDeleteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
