fe6a246473a972449957923b830e55b7f6d072ea
Rjchi <richardospina18@gmail.com>
2024-04-19 09:04:46 -0500

    [Feat] Agregar validación para un curso relacionado con una venta

--- src/app/modules/courses/courses-delete/courses-delete.component.ts
+++ src/app/modules/courses/courses-delete/courses-delete.component.ts
@@ -7,10 +7,9 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-courses-delete',
   templateUrl: './courses-delete.component.html',
-  styleUrls: ['./courses-delete.component.scss']
+  styleUrls: ['./courses-delete.component.scss'],
 })
 export class CoursesDeleteComponent implements OnInit {
-
   @Input() COURSE: any;
   @Output() CourseD: EventEmitter<any> = new EventEmitter();
 
@@ -24,14 +23,22 @@ export class CoursesDeleteComponent implements OnInit {
 
   delete() {
     this.courseService.removeCourses(this.COURSE._id).subscribe((res: any) => {
-      this.CourseD.emit('');
-      this.modal.close();
+      if (res&&res.code === 403) {
+        this.toaster.open({
+          text: 'NO SE PUEDO ELIMINAR EL CURSO',
+          caption: 'VALIDACIÓN',
+          type: 'danger',
+        });
+      } else {
+        this.CourseD.emit('');
+        this.modal.close();
 
-      this.toaster.open({
-        text: 'SE ELIMINO UN CURSO CORRECTAMENTE',
-        caption: 'VALIDACIÓN',
-        type: 'primary',
-      });
+        this.toaster.open({
+          text: 'SE ELIMINO UN CURSO CORRECTAMENTE',
+          caption: 'VALIDACIÓN',
+          type: 'primary',
+        });
+      }
     });
   }
 }
