2dda5c9a5ad1cfc907631c45c4b626de0ca5bd84
Rjchi <richardospina18@gmail.com>
2024-04-24 15:17:49 -0500

    [Feat] Agregar la opción de seleccionar un curso y una categoria para listar

--- src/app/modules/cupones/cupone-add/cupone-add.component.ts
+++ src/app/modules/cupones/cupone-add/cupone-add.component.ts
@@ -1,3 +1,4 @@
+import { Toaster } from 'ngx-toast-notifications';
 import { Component, OnInit } from '@angular/core';
 
 import { CuponesService } from '../service/cupones.service';
@@ -21,8 +22,10 @@ export class CuponeAddComponent implements OnInit {
 
   COURSES: any[] = [];
   CATEGORIES: any[] = [];
+  COURSES_SELECTED: any[] = [];
+  CATEGORIES_SELECTED: any[] = [];
 
-  constructor(public cuponeService: CuponesService) {}
+  constructor(public cuponeService: CuponesService, public toaster: Toaster) {}
 
   ngOnInit(): void {
     this.isLoading$ = this.cuponeService.isLoading$;
@@ -46,5 +49,65 @@ export class CuponeAddComponent implements OnInit {
 
   selectedTypeCupon(val: number) {
     this.type_cupon = val;
+  }
+
+  addCourse() {
+    if (this.course_id) {
+      let COURSE_INDEX = this.COURSES.findIndex(
+        (item: any) => item._id === this.course_id
+      );
+
+      if (COURSE_INDEX != -1) {
+        let VALID_EXIST_COURSE = this.COURSES_SELECTED.find(
+          (item: any) => item._id === this.course_id
+        );
+        if (VALID_EXIST_COURSE) {
+          this.toaster.open({
+            text: 'SELECCIONA OTRO CURSO (YA ESTA EN LA LISTA)',
+            caption: 'VALIDACIONES',
+            type: 'warning',
+          });
+          return;
+        } else {
+          this.COURSES_SELECTED.push(this.COURSES[COURSE_INDEX]);
+        }
+      }
+    } else {
+      this.toaster.open({
+        text: 'NECESITAS SELECCIONAR UN CURSO PARA PODER AGREGARLO',
+        caption: 'VALIDACIONES',
+        type: 'warning',
+      });
+    }
+  }
+
+  addCategorie() {
+    if (this.categorie_id) {
+      let CATEGORIE_INDEX = this.CATEGORIES.findIndex(
+        (item: any) => item._id === this.categorie_id
+      );
+
+      if (CATEGORIE_INDEX != -1) {
+        let VALID_EXIST_CATEGORIE = this.CATEGORIES_SELECTED.find(
+          (item: any) => item._id === this.categorie_id
+        );
+        if (VALID_EXIST_CATEGORIE) {
+          this.toaster.open({
+            text: 'SELECCIONA OTRA CATEGORIA (YA ESTA EN LA LISTA)',
+            caption: 'VALIDACIONES',
+            type: 'warning',
+          });
+          return;
+        } else {
+          this.CATEGORIES_SELECTED.push(this.CATEGORIES[CATEGORIE_INDEX]);
+        }
+      }
+    } else {
+      this.toaster.open({
+        text: 'NECESITAS SELECCIONAR UNA CATEGORIA PARA PODER AGREGARLA',
+        caption: 'VALIDACIONES',
+        type: 'warning',
+      });
+    }
   }
 }
