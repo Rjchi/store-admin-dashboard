15401365c0799c5f1bf044ba60600b0a71d26973
Rjchi <richardospina18@gmail.com>
2024-04-29 08:49:59 -0500

    [Feat] Registrar todos los componentes para el modulo discount

--- src/app/modules/discount/discount.module.ts
+++ src/app/modules/discount/discount.module.ts
@@ -3,11 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { DiscountRoutingModule } from './discount-routing.module';
 import { DiscountComponent } from './discount.component';
+import { DiscountAddComponent } from './discount-add/discount-add.component';
+import { DiscountEditComponent } from './discount-edit/discount-edit.component';
+import { DiscountDeleteComponent } from './discount-delete/discount-delete.component';
+import { DiscountListComponent } from './discount-list/discount-list.component';
 
 
 @NgModule({
   declarations: [
-    DiscountComponent
+    DiscountComponent,
+    DiscountAddComponent,
+    DiscountEditComponent,
+    DiscountDeleteComponent,
+    DiscountListComponent
   ],
   imports: [
     CommonModule,
