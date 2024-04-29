21a949279deb76a3030797398a0aed4bb177db74
Rjchi <richardospina18@gmail.com>
2024-04-29 14:00:09 -0500

    [Feat] Agregar la opción de eliminar un descuento

--- src/app/modules/discount/discount-delete/discount-delete.component.ts
+++ src/app/modules/discount/discount-delete/discount-delete.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+import { DiscountService } from '../service/discount.service';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Toaster } from 'ngx-toast-notifications';
 
 @Component({
   selector: 'app-discount-delete',
@@ -6,10 +10,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./discount-delete.component.scss']
 })
 export class DiscountDeleteComponent implements OnInit {
+  @Input() DISCOUNT: any;
+  @Output() DiscountD: EventEmitter<any> = new EventEmitter();
 
-  constructor() { }
+  constructor(
+    public toaster: Toaster,
+    public modal: NgbActiveModal,
+    public discountService: DiscountService
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  delete() {
+    this.discountService.removeDiscount(this.DISCOUNT._id).subscribe((res: any) => {
+      if (res&&res.code === 403) {
+        this.toaster.open({
+          text: 'NO SE PUEDO ELIMINAR EL DESCUENTO',
+          caption: 'VALIDACIÓN',
+          type: 'danger',
+        });
+      } else {
+        this.DiscountD.emit('');
+        this.modal.close();
+
+        this.toaster.open({
+          text: 'EL DESCUENTO SE ELIMINO CORRECTAMENTE',
+          caption: 'VALIDACIÓN',
+          type: 'primary',
+        });
+      }
+    });
   }
-
 }
