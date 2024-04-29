701b10efb1e022268e0b43275ec5ea5245068eeb
Rjchi <richardospina18@gmail.com>
2024-04-29 09:13:44 -0500

    [Feat] Agregar importaciones al modulo discount

--- src/app/modules/discount/discount.module.ts
+++ src/app/modules/discount/discount.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { InlineSVGModule } from 'ng-inline-svg-2';
+import { HttpClientModule } from '@angular/common/http';
+import { NgbModule, NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { DiscountRoutingModule } from './discount-routing.module';
 import { DiscountComponent } from './discount.component';
 import { DiscountAddComponent } from './discount-add/discount-add.component';
 import { DiscountEditComponent } from './discount-edit/discount-edit.component';
-import { DiscountDeleteComponent } from './discount-delete/discount-delete.component';
+import { DiscountRoutingModule } from './discount-routing.module';
 import { DiscountListComponent } from './discount-list/discount-list.component';
+import { DiscountDeleteComponent } from './discount-delete/discount-delete.component';
 
 
 @NgModule({
@@ -19,7 +23,14 @@ import { DiscountListComponent } from './discount-list/discount-list.component';
   ],
   imports: [
     CommonModule,
-    DiscountRoutingModule
+    DiscountRoutingModule,
+
+    NgbModule,
+    FormsModule,
+    NgbModalModule,
+    InlineSVGModule,
+    HttpClientModule,
+    ReactiveFormsModule,
   ]
 })
 export class DiscountModule { }
